d367862901c1781df895aa7e4a476485a35af3a1
fire-irror <firenoah.m@gmail.com>
2023-10-12 09:43:39 +0900

    ✨select음원 넣고 음악 화면 움직이기

--- src/project/ranking.java
+++ src/project/ranking.java
@@ -1,80 +1,79 @@
 package project;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class ranking extends JFrame {
-    private JTextField userIdField;
-    private JTextField scoreField;
-    private JButton saveButton;
 
-    public ranking() {
-        setTitle("게임 점수 저장");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 150);
+	private Image screenImage;
+	private Graphics screenGraphic;
+	private Image ranking = new ImageIcon(Main.class.getResource("../Images/rankingScreen.png")).getImage();
 
-        userIdField = new JTextField(10);
-        scoreField = new JTextField(10);
-        saveButton = new JButton("저장");
+	private ImageIcon rankBtn = new ImageIcon(Main.class.getResource("../Images/rankBtn.png"));
+	private ImageIcon darkRankBtn = new ImageIcon(Main.class.getResource("../Images/darkRankBtn.png"));
+	
+	private JButton rankbtn = new JButton(rankBtn);
+	private JButton darkRankbtn = new JButton(darkRankBtn);
+	
+	public ranking() {
+		setUndecorated(true);
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setBackground(new Color(0, 0, 0, 0));
+		setLayout(null);
+	}
 
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                saveScoreToDatabase();
-            }
-        });
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, this);
+		
+		 
+		rankbtn.setBounds(530, 250, 240, 500);
+		rankbtn.setBorderPainted(false);
+		rankbtn.setOpaque(false);
+		rankbtn.setContentAreaFilled(false);
+		rankbtn.setFocusPainted(false);	
+		
+		rankbtn.addMouseListener(new MouseAdapter() {
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 2));
-        panel.add(new JLabel("사용자 ID:"));
-        panel.add(userIdField);
-        panel.add(new JLabel("점수:"));
-        panel.add(scoreField);
-        panel.add(new JLabel()); // 빈 레이블
-        panel.add(saveButton);
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rankbtn.setIcon(darkRankBtn);
+				rankbtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rankbtn.setIcon(rankBtn);
+				rankbtn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
 
-        add(panel);
-    }
+			@Override
+			public void mousePressed(MouseEvent e) {
+			
+			}
+		});
 
-    private void saveScoreToDatabase() {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/score"; // 데이터베이스 URL 수정
-        String username = "root"; // 데이터베이스 사용자명 수정
-        String password = "0000"; // 데이터베이스 비밀번호 수정
+		add(rankbtn);
+	}
 
-        int userId = Integer.parseInt(userIdField.getText());
-        int score = Integer.parseInt(scoreField.getText());
+	public void screenDraw(Graphics g) {
+		g.drawImage(ranking, 0, 0, this);
 
-        try {
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-            String sql = "INSERT INTO score (user_id, score) VALUES (?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, userId);
-            preparedStatement.setInt(2, score);
-            preparedStatement.executeUpdate();
-            JOptionPane.showMessageDialog(this, "점수가 저장되었습니다.");
-            connection.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(this, "오류 발생: " + ex.getMessage(), "오류", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-////    public static void main(String[] args) {
-////        SwingUtilities.invokeLater(new Runnable() {
-////            public void run() {
-////                new ranking().setVisible(true);
-////            }
-////        });
-//    }
+		paintComponents(g);
+		this.repaint();
+	}
 }
-
-
-
-
-
-
