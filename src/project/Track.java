674a5f7a1fa08245c8ad73deffcba583e3f0be40
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2023-11-01 13:04:14 +0900

    [feat]완성

--- src/project/Track.java
+++ src/project/Track.java
@@ -1,11 +1,14 @@
 package project;
 
 public class Track {
-	private String titleImage;	//제목
-	private String startImage;
-	private String gameImage;
-	private String startMusic;
-	private String gameMusic;
+
+	private String titleImage; // 제목 부분 이미지
+	private String startImage; // 게임 선택 창 표지 이미지
+	private String gameImage; // 해당 곡을 실행했을 때 표지 이미지
+	private String startMusic; // 게임 선택 창 음악
+	private String gameMusic; // 해당 곡을 실행했을 때 음악
+	private String titleName; // 곡 제목
+	
 	public String getTitleImage() {
 		return titleImage;
 	}
@@ -36,13 +39,21 @@ public class Track {
 	public void setGameMusic(String gameMusic) {
 		this.gameMusic = gameMusic;
 	}
-	//public Track(String titleImage, String startImage, String gameImage, String startMusic, String gameMusic) {
-	public Track(String titleImage, String startImage,String startMusic, String gameMusic) {
+	public String getTitleName() {
+		return titleName;
+	}
+	public void setTitleName(String titleName) {
+		this.titleName = titleName;
+	}
+	
+	public Track(String titleImage, String startImage, String startMusic, String gameMusic, String titleName) {
 		super();
 		this.titleImage = titleImage;
 		this.startImage = startImage;
 		//this.gameImage = gameImage;
 		this.startMusic = startMusic;
 		this.gameMusic = gameMusic;
+		this.titleName = titleName;
 	}
+	
 }
